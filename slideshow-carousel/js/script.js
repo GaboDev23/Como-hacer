f459d2a9b2b41b64f9eebb1c83f563c345f1710b
Gabo <gabo.gh12398@gmail.com>
2023-12-10 15:54:36 -0300

    SlideHow / Carousel v0.4

--- slideshow-carousel/js/script.js
+++ slideshow-carousel/js/script.js
@@ -1,27 +1,84 @@
+let number = document.getElementById('number');
 let arrowLeft = document.getElementById('arrow-left');
 let arrowRight = document.getElementById('arrow-right');
 let images = document.querySelectorAll('.img');
+let buttons = document.querySelectorAll('.button');
+let caption = document.getElementById('caption');
+
+const opacidad = (image) => {
+    let computedStyle = getComputedStyle(image);
+    let opacity = computedStyle.getPropertyValue('opacity');
+
+    return opacity;
+}
+
+const establecerTexto = (image) => {
+    if (image == 0) return 'Caption Text';
+
+    if (image == 1) return 'Caption Two';
+
+    if (image == 2) return 'Caption Three';
+
+    if (image == 3) return 'Caption Four';
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        for (let j = 0; j < images.length; j++) {
+            let establecerOpacidad = opacidad(images[j]);
+
+            if (establecerOpacidad == '1') {
+                images[j].style.opacity = '0';
+                images[i].style.opacity = '1';
+                number.textContent = `${i+1} / 4`;
+                caption.textContent = establecerTexto(i);
+                break;
+            }
+            
+        }
+    });
+}
 
 arrowLeft.addEventListener('click', () => {
-    for (const i in images) {
-        console.log(i);
-        console.log(images[i]);
-        let computedStyle = window.getComputedStyle(images[i]);
-        let opacity = computedStyle.getPropertyValue('opacity');
-    
-        if (opacity == '1') {
-            images[i].style.opacity = '0';
+    for (let i = 0; i < images.length; i++) {
+        let establecerOpacidad = opacidad(images[i]);
 
-            if (images[i] == 0) {
-                let previousImage = images[images.length-1];
-                previousImage.style.opacity = '1';
-            }
+        if (establecerOpacidad == '1' && i == 0) {
+            number.textContent = `${images.length} / 4`;
+            images[i].style.opacity = '0';
+            images[images.length-1].style.opacity = '1';
+            caption.textContent = establecerTexto(images.length-1);
+            break;
+        } 
+
+        if (establecerOpacidad == '1' && i > 0) {
+            number.textContent = `${i} / 4`;
+            images[i].style.opacity = '0';
+            images[i-1].style.opacity = '1';
+            caption.textContent = establecerTexto(i-1);
+            break;
         }
     }
 });
 
 arrowRight.addEventListener('click', () => {
+    for (let i = 0; i < images.length; i++) {
+        let establecerOpacidad = opacidad(images[i]);
 
+        if (establecerOpacidad == '1' && i == images.length - 1) {
+            number.textContent = `1 / 4`;
+            images[i].style.opacity = '0';
+            images[0].style.opacity = '1';
+            caption.textContent = establecerTexto(0);
+            break;
+        } 
+
+        if (establecerOpacidad == '1' && i < images.length - 1) {
+            images[i].style.opacity = '0';
+            images[i+1].style.opacity = '1';
+            number.textContent = `${i+2} / 4`;
+            caption.textContent = establecerTexto(i+1);
+            break;
+        }
+    }
 });
-
-console.log(images);
